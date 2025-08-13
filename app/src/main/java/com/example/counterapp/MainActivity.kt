8e4d1849408e3402a528d35b5ee02dd6edd420f2
augustofa <augusto.fer.arantes@gmail.com>
2025-08-13 20:17:45 -0300

    layout adjustment and conditional enablement

--- app/src/main/java/com/example/counterapp/MainActivity.kt
+++ app/src/main/java/com/example/counterapp/MainActivity.kt
@@ -2,8 +2,8 @@ package com.example.counterapp
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,18 +19,26 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        checkPositiveValue(0)
     }
 
     fun addValue(view: View?){
-        var valueView = findViewById<TextView>(R.id.valueText)
-        val value = valueView.text.toString().toInt()
-        println("Value $value")
-        valueView.text = (value + 1).toString()
+        val valueView = findViewById<TextView>(R.id.valueText)
+        val newValue = valueView.text.toString().toInt() + 1
+        valueView.text = newValue.toString()
+        checkPositiveValue(newValue)
     }
 
     fun subValue(view: View?){
-        var valueView = findViewById<TextView>(R.id.valueText)
-        println("Sub")
-        valueView.setText(valueView.text.toString().toInt() - 1)
+        val valueView = findViewById<TextView>(R.id.valueText)
+        val newValue = valueView.text.toString().toInt() - 1
+        valueView.text = newValue.toString()
+        checkPositiveValue(newValue)
+    }
+
+    fun checkPositiveValue(value : Int){
+        val subButton = findViewById<Button>(R.id.subBtn);
+        subButton.isEnabled = (value > 0)
+        subButton.isClickable = (value > 0)
     }
 }
